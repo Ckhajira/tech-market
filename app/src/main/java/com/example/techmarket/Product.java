d5bf8bccad35335bf6905dd68dcea01c6053012a
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 14:38:11 +0300

    This commit creates a Product class

---
+++ app/src/main/java/com/example/techmarket/Product.java
@@ -0,0 +1,10 @@
+package com.example.techmarket;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+    private String imageUrl;
+    private String shortDesc;
+    private String longDesc;
+}
