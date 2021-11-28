ed1b1ebad30014bb71bfa4763b51cb121c3a0a8a
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 14:42:38 +0300

    This commit adds the getters and setters to the class

--- app/src/main/java/com/example/techmarket/Product.java
+++ app/src/main/java/com/example/techmarket/Product.java
@@ -16,4 +16,52 @@ public class Product {
         this.shortDesc = shortDesc;
         this.longDesc = longDesc;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getShortDesc() {
+        return shortDesc;
+    }
+
+    public void setShortDesc(String shortDesc) {
+        this.shortDesc = shortDesc;
+    }
+
+    public String getLongDesc() {
+        return longDesc;
+    }
+
+    public void setLongDesc(String longDesc) {
+        this.longDesc = longDesc;
+    }
 }
