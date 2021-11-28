970275c3963a8c583124f487ff3d30f361470ff5
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 15:35:55 +0300

    This commit adds the layout for the recyler adapter

--- app/src/main/java/com/example/techmarket/ProductRecViewAdapter.java
+++ app/src/main/java/com/example/techmarket/ProductRecViewAdapter.java
@@ -1,5 +1,14 @@
 package com.example.techmarket;
 
-public class ProductRecViewAdapter {
+import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductRecViewAdapter {
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
