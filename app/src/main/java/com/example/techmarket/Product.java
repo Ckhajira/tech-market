9c95f7daa2775f212845d4f8758ea5ad4acb5203
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 14:41:18 +0300

    This commit creates the product constructor

--- app/src/main/java/com/example/techmarket/Product.java
+++ app/src/main/java/com/example/techmarket/Product.java
@@ -7,4 +7,13 @@ public class Product {
     private String imageUrl;
     private String shortDesc;
     private String longDesc;
+
+    public Product(int id, String name, int price, String imageUrl, String shortDesc, String longDesc) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+    }
 }
