fddba4cc887777ff6a8d56be932ed6b312089444
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 14:58:51 +0300

    This commit initializes the recycler view

--- app/src/main/java/com/example/techmarket/AllProducts.java
+++ app/src/main/java/com/example/techmarket/AllProducts.java
@@ -1,14 +1,18 @@
 package com.example.techmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class AllProducts extends AppCompatActivity {
 
+    private RecyclerView productsRecView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_products);
+
+        productsRecView = findViewById(R.id.productsRecView);
     }
 }
