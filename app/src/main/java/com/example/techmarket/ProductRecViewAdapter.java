f1debe5cd487d293f513fc667bea58bc7a0e6959
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-28 15:53:14 +0300

    This commit inserts the three essential methods we must implement from the recyclerView that we extend

--- app/src/main/java/com/example/techmarket/ProductRecViewAdapter.java
+++ app/src/main/java/com/example/techmarket/ProductRecViewAdapter.java
@@ -1,14 +1,43 @@
 package com.example.techmarket;
 
+import android.media.Image;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProductRecViewAdapter {
+public class ProductRecViewAdapter extends RecyclerView.Adapter<ProductRecViewAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView parent;
+        private ImageView imgProduct;
+        private TextView txtName;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            parent = itemView.findViewById(R.id.parent);
+            imgProduct = itemView.findViewById(R.id.imgProduct);
+            txtName = itemView.findViewById(R.id.txtProductName);
         }
     }
 }
