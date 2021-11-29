b279d89af3113c2f2034a553bbbf7bdaa3081637
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-29 21:13:26 +0300

    This commit refactors the code

--- app/src/main/java/com/example/techmarket/MainActivity.java
+++ app/src/main/java/com/example/techmarket/MainActivity.java
@@ -6,22 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import butterknife.ButterKnife;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
-    private Button mLoginButton;
+   @BindView(R.id.loginButton) Button mLoginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLoginButton = (Button) findViewById(R.id.loginButton);
+        ButterKnife.bind(this);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, AllProductsActivity.class);
                 startActivity(intent);
+
             }
         });
     }
