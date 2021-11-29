014afd5b084094c9d6535669f981ec4b8bb624d9
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-29 13:11:36 +0300

    This commit adds a toast to check if the submit button works

--- app/src/main/java/com/example/techmarket/BuyFormActivity.java
+++ app/src/main/java/com/example/techmarket/BuyFormActivity.java
@@ -2,9 +2,12 @@ package com.example.techmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class BuyFormActivity extends AppCompatActivity {
 private EditText editTxtName, editTxtAddress, editTxtPhone;
@@ -18,5 +21,13 @@ private Button btnBuyFormSubmit;
         editTxtAddress = findViewById(R.id.editTxtAddress);
         editTxtPhone = findViewById(R.id.editTxtPhone);
         btnBuyFormSubmit = findViewById(R.id.btnBuyFormSubmit);
+
+        btnBuyFormSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(BuyFormActivity.this, SuccessActivity.class);
+//                startActivity(intent);
+                Toast.makeText(BuyFormActivity.this, "Hello World!", Toast.LENGTH_LONG).show();}
+        });
     }
 }
