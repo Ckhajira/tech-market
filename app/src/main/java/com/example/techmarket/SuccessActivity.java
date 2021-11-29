a61d83cfb2405d841bb0909fa63451cce28b22ec
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-29 13:50:34 +0300

    This commit sets the success text

---
+++ app/src/main/java/com/example/techmarket/SuccessActivity.java
@@ -0,0 +1,28 @@
+package com.example.techmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SuccessActivity extends AppCompatActivity {
+    private TextView txtIntro, txtFormDetails;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        Intent intent = getIntent();
+        String formName = intent.getStringExtra("formName");
+        String formAddress = intent.getStringExtra("formAddress");
+        String formPhone = intent.getStringExtra("formPhone");
+
+        txtIntro = findViewById(R.id.txtIntro);
+        txtFormDetails = findViewById(R.id.txtFormDetails);
+
+        txtFormDetails.setText("Hello " + formName + "We will deliver your package to " + formAddress);
+
+
+    }
+}
